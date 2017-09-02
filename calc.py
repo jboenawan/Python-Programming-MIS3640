8e42680c24b71034314e53c984b048fa2b9b8441
jboenawan <jboenawan1@babson.edu>
2017-09-01 22:10:32 -0400

    sudah satu

---
+++ calc.py
@@ -0,0 +1,20 @@
+#how many seconds is 42 minutes and 42 seconds?
+seconds = int(42*60+42)
+print(seconds)
+
+""" how many miles are there in 10 km? Hint: 1.61km
+equals to a mile."""
+
+miles = float(10/1.61)
+print(miles)
+
+#What is your average pace? (time per mile in minutes and sec)#
+pace_in_sec = (seconds/miles)
+print(pace_in_sec)
+pace_in_min = ((seconds/miles)/60)
+print(pace_in_min)
+
+
+#What is your average speed in miles per hour?#
+
+print((10/1.61)/2562*60*60)
