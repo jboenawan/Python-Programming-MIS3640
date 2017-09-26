4cb773a85243f7040200a1a61e416d92810ba574
jboenawan <jboenawan1@babson.edu>
2017-09-25 22:05:54 -0400

    session 7

---
+++ session7-exercise.py
@@ -0,0 +1,24 @@
+import math 
+
+def mysqrt(a)
+    while True:
+        print(x)
+        y=(x+a/x)/2
+        if y == x:
+            break
+        x=y
+
+def math(n):
+    n = float(n)
+    return math.sqrt(a)
+
+def test_square_root():
+    diff = mysqrt - math 
+    print "{:<12}\t {:<12}\t{}".format(mysqrt, math, diff)
+    for i in range(1,10):
+        m = mysqrt(i,x)
+        ma = math(i)
+        ab = abs(n-1)
+        print "{:<12}\t{:<12}\t{}".format(m, ma, ab)
+
+test_square_root()
