4c164c4aecd863c9e3fb2c01f5d5b98f45cb84ae
jboenawan <jboenawan1@babson.edu>
2017-10-12 22:07:32 -0400

    tes

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-def histogram(s):
-    d = dict()
-    for c in s:
-        d[c] = d.get(c,0) + 1 
-    return d
 
-h = histogram ('Bookbookkeeper')
-print(sorted(h))
-
-def print_hist():
-    for c in h:
-        print(c,h[c])
-
-print_hist()
